2e5d6d99cf6bd208fbbdf0560694cfeb63d29468
Ryan Silva <RyandotSilva@gmail.com>
2019-11-06 18:57:37 -0500

    Add SecurityConfiguration

---
+++ src/main/java/com/rdotsilva/financescraper/web/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.rdotsilva.financescraper.web.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/v1/stocks").permitAll();
+
+    }
+
+}
