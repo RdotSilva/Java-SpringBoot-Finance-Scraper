39bfb562106f3b63992ccf2ae4fa0b13f8be8955
Ryan Silva <RyandotSilva@gmail.com>
2019-11-06 18:43:38 -0500

    Add GetMapping for all stocks

--- src/main/java/com/rdotsilva/financescraper/web/controllers/StocksController.java
+++ src/main/java/com/rdotsilva/financescraper/web/controllers/StocksController.java
@@ -1,4 +1,21 @@
 package com.rdotsilva.financescraper.web.controllers;
 
+import com.rdotsilva.financescraper.web.models.Stock;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/stocks")
 public class StocksController {
+    @GetMapping
+    public List<Stock> list() {
+        List<Stock> stocks = new ArrayList<>();
+        return stocks;
+    }
+
 }
