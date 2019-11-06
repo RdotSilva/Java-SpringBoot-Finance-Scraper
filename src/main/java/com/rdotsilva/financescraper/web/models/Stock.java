ddcd688e484b112fb0503fc4feac52775ee9aa49
Ryan Silva <RyandotSilva@gmail.com>
2019-11-06 18:08:35 -0500

    Add Stock properties

--- src/main/java/com/rdotsilva/financescraper/web/models/Stock.java
+++ src/main/java/com/rdotsilva/financescraper/web/models/Stock.java
@@ -1,4 +1,15 @@
 package com.rdotsilva.financescraper.web.models;
 
+import java.util.Date;
+
 public class Stock {
+    private int scrapeId;
+    private Date scrapeDate;
+    private String symbol;
+    private String lastPrice;
+    private String changeAmount;
+    private String changePercent;
+    private String volume;
+    private String averageVolume;
+    private String marketCap;
 }
