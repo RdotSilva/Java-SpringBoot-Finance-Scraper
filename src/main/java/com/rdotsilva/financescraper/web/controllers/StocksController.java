cebf7ee80233f1000bde06175612a3af224ff258
Ryan Silva <RyandotSilva@gmail.com>
2019-11-06 19:13:37 -0500

    Update StocksController to use Stock repo

--- src/main/java/com/rdotsilva/financescraper/web/controllers/StocksController.java
+++ src/main/java/com/rdotsilva/financescraper/web/controllers/StocksController.java
@@ -1,6 +1,8 @@
 package com.rdotsilva.financescraper.web.controllers;
 
 import com.rdotsilva.financescraper.web.models.Stock;
+import com.rdotsilva.financescraper.web.repositories.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,20 +12,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/stocks")
 public class StocksController {
+    @Autowired
+    private StockRepository stockRepository;
+
     @GetMapping
     public List<Stock> list() {
-        List<Stock> stocks = new ArrayList<>();
-        return stocks;
+        return stockRepository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody Stock stock) {
+        stockRepository.save(stock);
     }
 
     @GetMapping("/{id}")
     public Stock get(@PathVariable("id") long id) {
-        return new Stock();
+        return stockRepository.getOne(id);
     }
 
 }
