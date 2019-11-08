2e6d2d8bdb6cffc9cbb3bf251ddedc676d074637
Ryan Silva <RyandotSilva@gmail.com>
2019-11-07 20:12:49 -0500

    Update StocksController with new RequestMapping

--- src/main/java/com/rdotsilva/financescraper/web/controllers/StocksController.java
+++ src/main/java/com/rdotsilva/financescraper/web/controllers/StocksController.java
@@ -4,31 +4,45 @@ import com.rdotsilva.financescraper.web.models.Stock;
 import com.rdotsilva.financescraper.web.repositories.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/v1/stocks")
+//@RestController
+//@RequestMapping("/api/v1/stocks")
+@Controller
 public class StocksController {
     @Autowired
     private StockRepository stockRepository;
 
-    @GetMapping
-    public List<Stock> list() {
-        return stockRepository.findAll();
+    @RequestMapping(value = "/stocks", method = RequestMethod.GET)
+    public String getAllStocks(Model model)
+    {
+        List<Stock> list = stockRepository.findAll();
+
+        model.addAttribute("stocks", list);
+        return "stocks";
+
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.OK)
-    public void create(@RequestBody Stock stock) {
-        stockRepository.save(stock);
-    }
-
-    @GetMapping("/{id}")
-    public Stock get(@PathVariable("id") long id) {
-        return stockRepository.getOne(id);
-    }
+//    @GetMapping
+//    public List<Stock> list() {
+//        return stockRepository.findAll();
+//    }
+//
+//
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.OK)
+//    public void create(@RequestBody Stock stock) {
+//        stockRepository.save(stock);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public Stock get(@PathVariable("id") long id) {
+//        return stockRepository.getOne(id);
+//    }
 
 }
