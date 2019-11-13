81c037d156c83d9cdd55b03fab1c5dd5d9f2149d
Ryan Silva <RyandotSilva@gmail.com>
2019-11-12 19:51:53 -0500

    Add BCrypt Bean

--- src/main/java/com/rdotsilva/financescraper/web/WebApplication.java
+++ src/main/java/com/rdotsilva/financescraper/web/WebApplication.java
@@ -2,10 +2,18 @@ package com.rdotsilva.financescraper.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class WebApplication {
 
 	public static void main(String[] args) { SpringApplication.run(WebApplication.class, args);}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
 
 }
