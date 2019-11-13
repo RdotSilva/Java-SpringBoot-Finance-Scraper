1bcf4fead570ae54c1c18dad6a0edf8acf42965d
Ryan Silva <RyandotSilva@gmail.com>
2019-11-12 19:36:32 -0500

    Add User properties

--- src/main/java/com/rdotsilva/financescraper/web/models/User.java
+++ src/main/java/com/rdotsilva/financescraper/web/models/User.java
@@ -1,4 +1,22 @@
 package com.rdotsilva.financescraper.web.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 }
